939b37d145730fd72343e863386d31dfe2d47a33
schoewed <dschoewe@rclco.com>
2021-09-16 10:03:12 -0400

    first data look

--- first_take.R
+++ first_take.R
@@ -1,4 +1,5 @@
 library(httr)
+library(tidyverse)
 #https://docs.google.com/document/d/1pSe1AeZXGL01m5uG3wwRr9k4pI2Qw52xzPi2NmNrhyI/edit#
 #https://docs.rstudio.com/connect/user/api-keys/
 
@@ -8,9 +9,16 @@ httr::GET(test)
 key <- "UL77gEnBSo166x5rDu9FG3Ao3MOBvOG879UqYQYq"
 url <- 'https://gs-api.greatschools.org/nearby-schools?lat=37.7940627&lon=-122.2680029&limit=3&distance=5'
 
-resp <- httr::GET(path = url,
-                  add_headers(Authorization = paste0("X-API-Key ", key)))
+resp <- httr::GET(url, add_headers("X-API-Key " = key))
 
 result <- httr::content(resp, as = "parsed")
 
+test <- result$schools
 
+test_school <- test[[2]]
+
+id <- as.data.frame(test_school[1])
+
+id2 <- as.data.frame(test_school[2])
+
+df <- bind_cols(id, id2)
